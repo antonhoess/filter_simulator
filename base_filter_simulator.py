refactor: Remove unnecessary test code in config.py. refactor: Rename setting to s in several constructors for make this code part easier to read. fix: Error by not dividing the covariance matrix by the GM-components total weight in the Panjer PHD filter. This was a mistake in the MATLAB code.

base_filter_simulator.py
@@ -56,23 +56,23 @@ class BaseFilterSimulator(FilterSimulator):
         # end
     # end
 
-    def __init__(self, settings: BaseFilterSimulatorConfigSettings) -> None:
-        self.__sim_loop_step_parts: List[SimStepPartBase] = settings.sim_loop_step_parts  # Needs to be set before calling the contructor of the FilterSimulator, since it already needs this values there
+    def __init__(self, s: BaseFilterSimulatorConfigSettings) -> None:
+        self.__sim_loop_step_parts: List[SimStepPartBase] = s.sim_loop_step_parts  # Needs to be set before calling the contructor of the FilterSimulator, since it already needs this values there
 
-        s = FilterSimulatorConfigSettings.from_obj(settings)
-        FilterSimulator.__init__(self, s)
+        s_sup = FilterSimulatorConfigSettings.from_obj(s)
+        FilterSimulator.__init__(self, s_sup)
 
         self.f = None
 
-        self._gospa_c: float = settings.gospa_c
-        self._gospa_p: int = settings.gospa_p
-        self._density_draw_style: DensityDrawStyleBase = settings.density_draw_style
-        self._n_bins_density_map: int = settings.n_bins_density_map
-        self._logging: Logging = settings.verbosity
-        self._draw_layers: Optional[List[DrawLayerBase]] = settings.draw_layers
-        self._show_legend: Optional[Union[int, str]] = settings.show_legend
-        self._fov: Limits = settings.fov
-        self._init_kbd_cmds = settings.init_kbd_cmds
+        self._gospa_c: float = s.gospa_c
+        self._gospa_p: int = s.gospa_p
+        self._density_draw_style: DensityDrawStyleBase = s.density_draw_style
+        self._n_bins_density_map: int = s.n_bins_density_map
+        self._logging: Logging = s.verbosity
+        self._draw_layers: Optional[List[DrawLayerBase]] = s.draw_layers
+        self._show_legend: Optional[Union[int, str]] = s.show_legend
+        self._fov: Limits = s.fov
+        self._init_kbd_cmds = s.init_kbd_cmds
         # --
         self._active_draw_layers: List[bool] = [True for _ in self._draw_layers]
         self._ext_states: List[List[np.ndarray]] = list()

gm.py
@@ -331,21 +331,20 @@ class Gmm:
             comp.weight += summand
     # end def
 
-    def get_unified_comp(self, norm_cov=True):
+    def get_unified_comp(self):
         # Create unified new component from subsumed ones
         agg_weight = self.get_total_weight()
 
+        # Mean
         loc = np.sum(np.array([comp.weight * comp.loc for comp in self]), 0) / agg_weight
 
+        # Covariance
         cov = 0
         for comp in self:
             dist = loc - comp.loc  # The original implementation used the weightiest component instead of the new location calculated just above
-            cov += comp.weight * (comp.cov + np.dot(dist, dist.T))  # Why is no sqrt() used? # Wrong implementation in the original code using "dist * dist.T" instead of np.dot()
+            cov += comp.weight * (comp.cov + np.dot(dist, dist.T))  # Wrong implementation in the original code using "dist * dist.T" instead of np.dot()
         # end for
-
-        if norm_cov:
-            cov /= agg_weight
-        # end if
+        cov /= agg_weight
 
         return GmComponent(agg_weight, loc, cov)
 # end class